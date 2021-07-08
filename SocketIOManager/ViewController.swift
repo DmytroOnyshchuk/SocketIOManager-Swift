e800fb6bd135be0718628d0268192ad3d743a06c
Dmitry Onishchuk <dmitry.oni90@gmail.com>
2021-07-08 19:40:59 +0300

    Initial Commit

---
+++ SocketIOManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocketIOManager
+//
+//  Created by Dmitry Onishchuk on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
